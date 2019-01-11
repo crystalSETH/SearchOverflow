857179c01c62bd6ff8a13dbec34880d6d5e13d64
Seth Folley <sethfolley@gmail.com>
2019-01-11 01:32:39 -0600

    Initial Commit

---
+++ SearchOverflow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchOverflow
+//
+//  Created by Seth Folley on 1/11/19.
+//  Copyright © 2019 Seth Folley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
