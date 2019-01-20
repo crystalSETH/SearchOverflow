89f0d75d6acf852dc8196a8e5ae028306279f830
Seth Folley <sethfolley@gmail.com>
2019-01-20 15:55:35 -0600

    Continuing previous, premature, commit.

--- SearchOverflowTests/Data/API/StackOverflowResponseTests.swift
+++ SearchOverflowTests/Data/API/StackOverflowResponseTests.swift
@@ -7,27 +7,31 @@
 //
 
 import XCTest
+@testable import SearchOverflow
 
 class StackOverflowResponseTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    let decoder = JSONDecoder()
+    
+    override func setUp() { }
+    
+    override func tearDown() { }
+    
+    func test_Decodable_StackOResponseDecodingSucceeds() {
+        guard let data = SearchOverflowTests.loadJSON(named: "StackOResponseDecodeSuccess") else {
+            XCTFail()
+            return
         }
+        
+        XCTAssertNoThrow(try decoder.decode(StackOverflowResponse<Answer>.self, from: data))
     }
-
+    
+    func test_Decodable_StackOResponseDecodingFails() {
+        guard let data = SearchOverflowTests.loadJSON(named: "StackOResponseDecodeFailure") else {
+            XCTFail()
+            return
+        }
+        
+        XCTAssertThrowsError(try decoder.decode(StackOverflowResponse<Answer>.self, from: data))
+    }
 }
