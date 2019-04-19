9c17cc9ce8dac1d0c67e02bf520d29735bb9fba2
Seth Folley <seth.folley@carium.com>
2019-04-19 16:23:07 -0500

    QuestionDetailsVC tests. Refined setup, consts, and vars

--- SearchOverflowTests/View Controllers/DetailViewControllerTests.swift
+++ SearchOverflowTests/View Controllers/DetailViewControllerTests.swift
@@ -12,19 +12,38 @@ import XCTest
 class DetailViewControllerTests: XCTestCase {
 
     var sut: QuestionDetailsViewController!
-    
+    private lazy var question: Question = {
+        let question = Question(id: 1, owner: nil, score: 100, title: "Huckle", body: "Berry",
+                                    createdOn: Date(), tags: [], viewCount: 100, isAnswered: true,
+                                    answers: testAnswers, acceptedAnswerId: 3, answerCount: 4)
+        
+        return question
+    }()
+
+    private let testAnswers = [ Answer(id: 1, owner: User(id: 1, displayName: "John Stark", type: .registered,
+                                                          reputation: 42, profileImageUrl: "https://avatars1.githubusercontent.com/u/7024589?s=460&v=4"),
+                                       score: 100, title: "Howdy", body: "Doody",
+                                       createdOn: Date(), tags: [], isAccepted: false, questionId: 1),
+                                Answer(id: 2, owner: nil, score: 90, title: "", body: "",
+                                       createdOn: Date(), tags: [], isAccepted: false, questionId: 1),
+                                Answer(id: 3, owner: nil, score: 80, title: "", body: "",
+                                       createdOn: Date(), tags: [], isAccepted: true, questionId: 1),
+                                Answer(id: 4, owner: nil, score: 70, title: "", body: "",
+                                       createdOn: Date(), tags: [], isAccepted: false, questionId: 1)
+                               ]
+
     override func setUp() {
-        guard let questionDetailsVC = UIStoryboard(name: "QuestionDetails", bundle: nil)
-            .instantiateViewController(withIdentifier: "QuestionDetailsViewController") as? QuestionDetailsViewController
+        guard let questionDetailsVC = UIStoryboard(name: "QuestionDetails", bundle: nil).instantiateInitialViewController() as? QuestionDetailsViewController
         else {
             XCTFail()
             return
         }
         
         sut = questionDetailsVC
+        sut.question = question
         sut.loadViewIfNeeded()
     }
-    
+
     override func tearDown() {
         sut = nil
     }
@@ -44,28 +63,34 @@ class DetailViewControllerTests: XCTestCase {
         XCTAssertNotNil(sut.dismissButton)
     }
     
+    // MARK: Table View Tests
     func test_QDVC_IsAnswerTableDatasource() {
-        
         XCTAssertEqual(sut, sut.answersTableView.dataSource as? QuestionDetailsViewController)
     }
     
     func test_QDVC_AnswerTableNumberOfRows() {
-        let question = Question(id: 1, owner: nil, score: 100, title: "Huckle", body: "Berry",
-                                createdOn: Date(), tags: [], viewCount: 100, isAnswered: true,
-                                answers: testAnswers, acceptedAnswerId: 3, answerCount: 4)
-        
-        sut.question = question
-
         XCTAssertEqual(question.answers?.count, sut.answersTableView.numberOfRows(inSection: 0))
     }
-}
+    
+    func test_QDVC_AnswerTableNumberOfRows_nilQuestion() {
+        sut.question = nil
+        XCTAssertEqual(sut.answersTableView.numberOfRows(inSection: 0), 0)
+    }
+    
+    func test_QDVC_AnswerTableCellForRowAt() {
+        let answer = (sut.question?.answers?.first)!
+        let cell = sut.tableView(sut.answersTableView, cellForRowAt: IndexPath(item: 0, section: 0)) as! AnswerCell
+        XCTAssertEqual(cell.usernameLabel.text, answer.owner!.displayName)
+        XCTAssertNotNil(cell.gravatarImage.kf.placeholder)
+    }
+    
+    func test_QDVC_AnswerTableCellForRowAt_nilOwner() {
+        let cell = sut.tableView(sut.answersTableView, cellForRowAt: IndexPath(item: 1, section: 0)) as! AnswerCell
+        XCTAssertEqual(cell.usernameLabel.text, QuestionDetails.defaultUsername)
+    }
 
-private let testAnswers = [ Answer(id: 1, owner: nil, score: 100, title: "", body: "",
-                                   createdOn: Date(), tags: [], isAccepted: false, questionId: 1),
-                            Answer(id: 2, owner: nil, score: 90, title: "", body: "",
-                                   createdOn: Date(), tags: [], isAccepted: false, questionId: 1),
-                            Answer(id: 3, owner: nil, score: 80, title: "", body: "",
-                                   createdOn: Date(), tags: [], isAccepted: true, questionId: 1),
-                            Answer(id: 4, owner: nil, score: 70, title: "", body: "",
-                                   createdOn: Date(), tags: [], isAccepted: false, questionId: 1)
-                          ]
+    func test_QDVC_AnswerTableCellForRowAt_nilQuestion() {
+        let cell = sut.answersTableView.cellForRow(at: IndexPath(item: 0, section: 0))
+        XCTAssertFalse(cell is AnswerCell)
+    }
+}
