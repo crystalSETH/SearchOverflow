f615d99ebba3ad04b7c684740e6eea4d6f229484
Seth Folley <sethfolley@gmail.com>
2019-04-22 18:23:01 -0500

    Additional app coordinator test

--- SearchOverflowTests/Coordinators/AppCoordinatorTests.swift
+++ SearchOverflowTests/Coordinators/AppCoordinatorTests.swift
@@ -14,13 +14,27 @@ class AppCoordinatorTests: XCTestCase {
 
     override func setUp() {
         sut = AppCoordinator(navigationController: UINavigationController())
+        sut?.begin()
     }
     
-    func test_CoordinatorBegins() {
-        sut?.begin()
-        
+    func test_CoordinatorBegan() {
         if sut?.navController.presentedViewController == nil, sut?.navController.viewControllers.isEmpty != false {
             XCTFail()
         }
+    }
+    
+    func test_DisplaysQuestionDetails() {
+        let question = Question(id: 0, owner: nil, score: 0, title: "", body: "", createdOn: Date(),
+                                tags: [], viewCount: 0, isAnswered: false, answers: nil, acceptedAnswerId: nil, answerCount: 0)
+        sut?.viewQuestionDetails(question)
+
+        let expectation = XCTestExpectation(description: "QuestionDetails is displayed")
+        DispatchQueue.global().asyncAfter(deadline: .now() + 1.0) {
+            if self.sut?.navController.topViewController is QuestionDetailsViewController {
+                expectation.fulfill()
+            }
+        }
+        
+        wait(for: [expectation], timeout: 3.0)
     }
 }
