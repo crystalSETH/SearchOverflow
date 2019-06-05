43d5e299c2fe1cdd29259fabe92f889966424e6c
Seth Folley <sethfolley@gmail.com>
2019-06-05 01:23:27 -0500

    Answers table VC

---
+++ SearchOverflow/View Controllers/Question Details/AnswersTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AnswersTableViewController.swift
+//  SearchOverflow
+//
+//  Created by Seth Folley on 6/5/19.
+//  Copyright © 2019 Seth Folley. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AnswersTableViewController: UITableViewController {
+
+}
